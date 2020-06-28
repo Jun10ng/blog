964db9584e493415567b6bec5073d42951c9af53
Jun10ng <fu4ng@foxmail.com>
2020-06-28 18:15:34 +0800

    更新

--- hegit.sh
+++ hegit.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 echo -e "清除旧文件"
 hexo cl
 echo -e "\t\t......OK\n正在【生成】静态页面"
@@ -9,12 +10,20 @@ sh book/book.sh
 # end git book
 echo -e "\t\t......OK\n正在【发布】静态页面"
 hexo d 
-echo -e "\t\t......OK\n将源文件添加到git版本库"
-git add .  
-echo -e "\t\t......OK\n执行git-commit"
-git commit -m "$1" 
-git push  
-echo -e "\t\t......OK\n执行git-push"
 
-echo "发布完成"
+if [ -z "$1" ];
+then
+# $1为空只更新静态网页，不git提交
+    echo "更新完毕"
+else
+    git add .  
+    echo -e "\t\t......OK\n执行git-commit"
+    git commit -m "$1" 
+    git push  
+    echo -e "\t\t......OK\n执行git-push"
+
+    echo "发布完成"
+fi
+
+
 
--- source/books/index.md
+++ source/books/index.md
@@ -1,8 +1,9 @@
 ---
-title: books
+title: 系列文章收录
 date: 2020-06-28 17:20:41
 ---
+![](https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1593346660760&di=71bb9d6627a99f724917e290902862ea&imgtype=0&src=http%3A%2F%2Fimages.trvl-media.com%2Fmedia%2Fcontent%2Fshared%2Fimages%2Ftravelguides%2Fdestination%2F6046241%2FLiverpool-Central-Library-72899.jpg)
 
-把系列文章收录成书，欢迎指出错误，十分感谢。
+欢迎指出错误，十分感谢。
 
 [《Go设计模式与并发模式》](http://jun10ng.work/book/gdpcp/)

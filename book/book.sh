b37fefa9777835effcac7e40b4a823a91c4bcf7c
Jun10ng <fu4ng@foxmail.com>
2020-06-28 17:24:52 +0800

    添加 books 导航

--- book/book.sh
+++ book/book.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd book
 
 source=../public/book
 for dir in *;
--- hegit.sh
+++ hegit.sh
@@ -3,6 +3,10 @@ echo -e "清除旧文件"
 hexo cl
 echo -e "\t\t......OK\n正在【生成】静态页面"
 hexo g 
+# gitbook
+echo -e "\t\t......OK\n正在处理gitbook文件夹"
+sh book/book.sh
+# end git book
 echo -e "\t\t......OK\n正在【发布】静态页面"
 hexo d 
 echo -e "\t\t......OK\n将源文件添加到git版本库"

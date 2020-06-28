4b02c98349d1fe21bef3e99838c72346a4826fe3
Jun10ng <fu4ng@foxmail.com>
2020-06-28 16:09:49 +0800

    2020-06-28 16:09.49 发布文章:

---
+++ book/book.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source=../public/book
+for dir in *;
+do
+  #删除gitbook生成的内容
+  if [ -d $source/$dir ]; then
+    rm -r $source/$dir
+  fi
+  #重新生成gitbook的内容
+  if [ -d $dir ]; then
+    gitbook build $dir $source/$dir
+  fi
+done
